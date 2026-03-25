6a869d6957356a5a8b9b8f7a2b59b551dee9d416
izzy666cn <>
2026-03-25 15:57:07 +0800

    up

--- keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
+++ keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
@@ -117,14 +117,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | SHIFT|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | SHIFT |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | MO(4)| MO(5)| LAlt | LGUI |Lower |    Space    |Raise |LCAG  |LCtrl |  Up  |Right |
+ * | MO(4)| MO(5)| LAlt | LGUI |Lower |    Space    |Raise |LCAG  |LAlt |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT_planck_mit(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_EQL,
     KC_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,   KC_SCLN,  TD(TD_QUOT_DQUO),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-    MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  LCAG(KC_NO),  KC_LCTL,  KC_UP,   KC_RGHT
+    MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  LCAG(KC_NO),  KC_LAlt,  KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | ` |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |      |      |      |      |  \   |
+ * | Del  |      |      |      |CAG(F)|      |      |      |      |      |      |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |Ctrl+X|Ctrl+C|      |      |      |      |      |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Ctrl+X Ctrl+C for Emacs
 [2] = LAYOUT_planck_mit(
     _______,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_GRV,
-    KC_DEL,   _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BSLS,
+    KC_DEL,   _______,  _______,   _______,  LCAG(KC_F),  _______,  _______,  _______,  _______,  _______,  _______,  KC_BSLS,
     _______,  _______,  LCTL(KC_X),LCTL(KC_C),  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_PGDN,  _______,
     _______,  _______,  _______,  _______,  _______,  KC_ENTER,  _______,  _______,  _______,  _______,  _______
 ),
