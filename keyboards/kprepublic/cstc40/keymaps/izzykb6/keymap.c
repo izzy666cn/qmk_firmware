c497eb26b97a9c239c37fe8b1bd78a850df0cb88
izzy666cn <>
2026-03-25 15:58:01 +0800

    up

--- keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
+++ keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_EQL,
     KC_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,   KC_SCLN,  TD(TD_QUOT_DQUO),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-    MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  LCAG(KC_NO),  KC_LAlt,  KC_UP,   KC_RGHT
+    MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  LCAG(KC_NO),  KC_LALT,  KC_UP,   KC_RGHT
 ),
 
 /* Lower
