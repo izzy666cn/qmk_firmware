8a4ea7671c9e0ea70eaa6787fa8cda8b3309a55b
izzy666cn <>
2026-04-13 14:32:02 +0800

    emacs 风格的方向键

--- keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
+++ keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
@@ -122,27 +122,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [0] = LAYOUT_planck_mit(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_EQL,
-    KC_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,     KC_J,     KC_K,     KC_L,   KC_SCLN,  TD(TD_QUOT_DQUO),
+    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  LCTL_T(KC_QUOT),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
     MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  LCAG(KC_NO),  KC_LALT,  KC_UP,   KC_RGHT
 ),
-
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |  ~/  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   -  |   +  |   ~  |
+ * |  ~/  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   -  |  Up  |   ~  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Caps |      |      |      |      |      |   {  |   (  |   )  |   }  |   _  | pipe |
+ * | Caps |      |      |      |Right |      |   {  |   (  |   )  |   }  |   +  | pipe |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      | GUI+X|      |      |      |      |   [  |   ]  |      |  ->  |  =>  |
+ * |      |      | GUI+X|      |      | Left | Down |   [  |   ]  |      |  ->  |  =>  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |  Backspace  |      | Left | Down |  Up  | Right|
  * `-----------------------------------------------------------------------------------'
  */
 // GUI+X for Emacs(M-x)
 [1] = LAYOUT_planck_mit(
-    TILD_SLSH,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,     KC_ASTR,     KC_MINS,  KC_PLUS,  KC_TILD,
-    KC_CAPS,  _______,  _______,   _______,  _______, _______,  KC_LCBR,  KC_LPRN,     KC_RPRN,     KC_RCBR,   KC_UNDS,  KC_PIPE,
-    _______,  _______,  LGUI(KC_X),  _______,  _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  _______,  THIN_ARROW, FAT_ARROW,
+    TILD_SLSH,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,     KC_ASTR,     KC_MINS,  KC_UP,  KC_TILD,
+    KC_CAPS,  _______,  _______,   _______,  KC_RIGHT, _______,  KC_LCBR,  KC_LPRN,     KC_RPRN,     KC_RCBR,   KC_PLUS,  KC_PIPE,
+    _______,  _______,  LGUI(KC_X),  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_LBRC,  KC_RBRC,  _______,  THIN_ARROW, FAT_ARROW,
     _______,  _______,  _______,  _______,  _______,  KC_BSPC,  _______,  KC_LEFT,     KC_DOWN,     KC_UP,  KC_RIGHT
 ),
 /* Raise
