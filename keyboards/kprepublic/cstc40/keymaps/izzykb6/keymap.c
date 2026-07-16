3bceb5149c51c5bbed616dafb6758c0ffaf79d57
izzy666cn <>
2026-07-16 15:11:16 +0800

    up

--- keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
+++ keyboards/kprepublic/cstc40/keymaps/izzykb6/keymap.c
@@ -135,14 +135,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | SHIFT|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | SHIFT |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | MO(4)| MO(5)| LAlt | LGUI |Lower |    Space    |Raise |LCAG  |LAlt |  Up  |Right |
+ * | MO(4)| MO(5)| LAlt | LGUI |Lower |    Space    |Raise |RGUI  |LAlt |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT_planck_mit(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_EQL,
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  LCTL_T(KC_QUOT),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
-    MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  LCAG(KC_NO),  KC_LALT,  KC_UP,   KC_RGHT
+    MO(4),    MO(5),    KC_LALT,  KC_LGUI,  TL_LOWR,  KC_SPC,  TL_UPPR,  KC_RGUI,  KC_LALT,  KC_UP,   KC_RGHT
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
